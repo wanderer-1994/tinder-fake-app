a3fb6c90ca0ca999983ae1f0f417f7db1d82166a
TKH <trankhachuy94@gmail.com>
2022-03-09 16:50:37 +0700

    Rebuild app entry file & Add Navigation bar

---
+++ component/StaticLink.ts
@@ -0,0 +1,19 @@
+interface StaticLink {
+    href: any,
+    label: String
+}
+
+const Links: StaticLink[] = [
+    {
+        href: '/pass',
+        label: 'Pass'
+    }, {
+        href: '/',
+        label: 'Discover'
+    }, {
+        href: '/like',
+        label: 'Like'
+    }
+]
+
+export default Links
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../component/Navbar'
+import { Provider } from 'react-redux'
+import store from '../redux/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <div className="app-wrapper">
+        <Component {...pageProps} />
+        <Navbar />
+      </div>
+    </Provider>
+  )
 }
 
 export default MyApp
