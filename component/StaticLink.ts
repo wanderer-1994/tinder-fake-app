9af94929a4e9d5b797aa4390d74d85544179d17e
TKH <trankhachuy94@gmail.com>
2022-03-16 18:33:07 +0700

    Fix lint error

--- component/StaticLink.ts
+++ component/StaticLink.ts
@@ -1,19 +1,21 @@
 interface StaticLink {
-    href: any,
-    label: String
+  href: any;
+  label: String;
 }
 
 const Links: StaticLink[] = [
-    {
-        href: '/pass',
-        label: 'Pass'
-    }, {
-        href: '/',
-        label: 'Discover'
-    }, {
-        href: '/like',
-        label: 'Like'
-    }
-]
+  {
+    href: '/pass',
+    label: 'Pass',
+  },
+  {
+    href: '/',
+    label: 'Discover',
+  },
+  {
+    href: '/like',
+    label: 'Like',
+  },
+];
 
-export default Links
+export default Links;
