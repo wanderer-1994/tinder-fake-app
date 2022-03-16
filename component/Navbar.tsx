e041d5041d5bef5813fe61de4c1973a9afb182c2
TKH <trankhachuy94@gmail.com>
2022-03-16 18:32:34 +0700

    Fix lint error

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -1,71 +1,77 @@
-import { NextComponentType } from 'next'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import { NextComponentType } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import styles from '../styles/component/Navbar.module.css';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import { InitialState } from '../redux/store';
 import Links from './StaticLink';
 
 const Navbar: NextComponentType = (props: any) => {
-    let router = useRouter();
-    useEffect(() => {
-        let currentUserId = localStorage.getItem('currentUserId');
-        if (!currentUserId) {
-            fetch('/api/user')
-                .then(res => res.json())
-                .then(data => {
-                    if (data && data.data && data.data.length) {
-                        let index = Math.floor(Math.random() * (data.data.length - 1));
-                        let currentUser = data.data[index];
-                        localStorage.setItem('currentUserId', currentUser._id);
-                        return props.dispatch({
-                            type: 'UPDATE_USER',
-                            payload: currentUser
-                        })
-                    }
-                    return alert('There are no user in database')
-                })
-                .catch(err => {
-                    alert(err.message);
-                })
-        } else {
-            fetch(`/api/user/${currentUserId}`)
-                .then(res => res.json())
-                .then(data => {
-                    if (data.user) {
-                        return props.dispatch({
-                            type: 'UPDATE_USER',
-                            payload: data.user
-                        })
-                    }
-                    throw new Error('User not found')
-                })
-                .catch(err => {
-                    localStorage.setItem('currentUserId', '');
-                    location.reload();
-                })
-        }
-    }, [])
+  const router = useRouter();
+  useEffect(() => {
+    const currentUserId = localStorage.getItem('currentUserId');
+    if (!currentUserId) {
+      fetch('/api/user')
+        .then((res) => res.json())
+        .then((data) => {
+          if (data && data.data && data.data.length) {
+            const index = Math.floor(Math.random() * (data.data.length - 1));
+            const currentUser = data.data[index];
+            localStorage.setItem('currentUserId', currentUser._id);
+            return props.dispatch({
+              type: 'UPDATE_USER',
+              payload: currentUser,
+            });
+          }
+          return alert('There are no user in database');
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    } else {
+      fetch(`/api/user/${currentUserId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.user) {
+            return props.dispatch({
+              type: 'UPDATE_USER',
+              payload: data.user,
+            });
+          }
+          throw new Error('User not found');
+        })
+        .catch(() => {
+          localStorage.setItem('currentUserId', '');
+          location.reload();
+        });
+    }
+  }, []);
 
-    return (
-        <div className={styles.navWrapper}>
-            {Links.map((item, index) => {
-                let isActive = router.pathname === item.href;
-                return (
-                    <Link key={index} href={item.href}>
-                        <a className={[styles.navItem, isActive ? styles.active : ''].join(' ')}>{item.label}</a>
-                    </Link>
-                )
-            })}
-        </div>
-    )
-}
+  return (
+    <div className={styles.navWrapper}>
+      {Links.map((item, index) => {
+        const isActive = router.pathname === item.href;
+        return (
+          <Link key={index} href={item.href}>
+            <a
+              className={[styles.navItem, isActive ? styles.active : ''].join(
+                ' '
+              )}
+            >
+              {item.label}
+            </a>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
 function mapStateToProps(state: InitialState): InitialState {
-    return {
-        currentUser: state.currentUser
-    }
+  return {
+    currentUser: state.currentUser,
+  };
 }
 
-export default connect(mapStateToProps)(Navbar)
+export default connect(mapStateToProps)(Navbar);
